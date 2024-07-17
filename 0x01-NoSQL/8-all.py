9981914b985763f4c43d5d1f2c1fb7b24998241a
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-17 05:59:47 +0100

    return list using python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" returns a list from mongo collections"""
+
+
+def list_all(mongo_collection):
+    """returns a list from mongo collections"""
+    newlist = []
+    for doc in mongo_collection.find():
+        newlist.append(mongo_collection)
+    return newlist
