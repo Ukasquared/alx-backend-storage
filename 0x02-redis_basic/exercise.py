7215e77c786484b621bf512e2c5a9f6f255bf8b1
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-18 04:55:53 +0000

    convert redis data to object

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """writing strings to redis"""
 import redis
-from typing import Union
+from typing import Union, Callable
 import uuid
 
 class Cache:
@@ -9,11 +9,29 @@ class Cache:
     def __init__(self):
         """ create an instance of redis """
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
     def store(self, data: Union[str, int,
               bytes, float]) -> str:
         """ store string to redis db"""
         id = str(uuid.uuid4())
         self._redis.set(id, data)
-        return id    
+        return id
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, float, int, bytes]:
+        """ get value from the database"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """convert to string"""
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: int) -> int:
+        """convert to int"""
+        return self.get(key, int(lambda x: x.decode('utf-8')))
