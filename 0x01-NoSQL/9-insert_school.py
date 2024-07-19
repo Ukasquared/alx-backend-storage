c43067f4d02ec65e8d9cd0673b89432ad899ee15
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-07-19 18:56:55 +0100

    find the last key-value in the doc and return the id

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""inserts a new document
+in a collection """
 
 
+def insert_school(mongo_collection, **kwargs):
+    """inserts a new
+    document in a collection"""
+    mongo_collection.insert_one({**kwargs})
+    new_id = mongo_collection.find().sort("_id", -1).limit(1).next()
+    return new_id['_id']
