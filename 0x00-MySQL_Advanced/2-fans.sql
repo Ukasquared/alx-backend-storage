08517e9549e70b0f6d4731c1590064e4305b9846
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-08-12 14:56:48 +0100

    get number of commited fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,7 @@
 -- script that ranks country origins of bands, ordered by the number of (non-unique) fans
+ALTER TABLE metal_bands
+CHANGE COLUMN fans nb_fans INT;
+
 SELECT origin, SUM(nb_fans) AS Total
 FROM metal_bands
 GROUP BY origin
